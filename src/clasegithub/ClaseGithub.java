a85820940b96cfeffc085fc6250803c5315a2f15
sebasmillan339-stack <sebasmillan339@gmail.com>
2026-09-16 09:15:37 -0500

    Se hizo otro print en la rama JuanMillan

--- src/clasegithub/ClaseGithub.java
+++ src/clasegithub/ClaseGithub.java
@@ -15,8 +15,9 @@ public class ClaseGithub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
+        System.out.println("Hello World1!");
+        System.out.println("Hello World2!");
     }
     
 }
